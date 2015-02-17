76a80fd8066c4b071ddded952be6acb39818df02
Andy Martha <andrewpmartha@gmail.com>
2015-02-17 16:59:42 -0600

    first commit after working port to backdrop 1.0

--- auto_nodetitle.js
+++ auto_nodetitle.js
@@ -1,20 +1,20 @@
 (function ($) {
 
-Drupal.behaviors.auto_nodetitleFieldsetSummaries = {
+Backdrop.behaviors.auto_nodetitleFieldsetSummaries = {
   attach: function (context) {
-    $('fieldset#edit-auto-nodetitle', context).drupalSetSummary(function (context) {
+    $('fieldset#edit-auto-nodetitle', context).backdropSetSummary(function (context) {
 
       // Retrieve the value of the selected radio button
-      var ant = $("input[@name=#edit-auto-nodetitle-ant]:checked").val();
+      var ant = $("input[name=#edit-auto-nodetitle-ant]:checked").val();
 
       if (ant==0) {
-        return Drupal.t('Disabled')
+        return Backdrop.t('Disabled')
       }
       else if (ant==1) {
-        return Drupal.t('Automatic (hide title field)')
+        return Backdrop.t('Automatic (hide title field)')
       }
       else if (ant==2) {
-        return Drupal.t('Automatic (if title empty)')
+        return Backdrop.t('Automatic (if title empty)')
       }
     });
   }
